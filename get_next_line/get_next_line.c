8351ac3f02a83acf4b16301850d3b431a7fd670b
root <root@CMD-20220513.localdomain>
2022-07-07 02:03:16 +0900

    gnl 7/7

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -13,6 +13,36 @@
 
 #include "get_next_line.h"
 
+char    *get_line(char *st_variable)
+{
+    char    *line;
+    size_t  i;
+
+    i = 0;
+    while (st_variable[i] != '\n' && st_variable[i])
+        ++i;
+    if (st_variable[i] == '\n')
+        ++i;
+    
+    line = (char *)malloc(sizeof(char) * (i+1));
+    if (!line)
+        return (0);
+    
+    i = 0;
+    while (st_variable[i] != '\n' && st_variable[i])
+    {
+        line[i] = st_variable[i];
+        ++i;
+    }
+    if (st_variable[i] == '\n')
+    {
+        line[i] = '\n';
+        ++i;
+    }
+    line[i] = 0; // 뭐야??
+    return (line);
+}
+
 char    *read_file(int fd, char *st_variable)
 {
     char    *buff;
@@ -35,7 +65,7 @@ char    *read_file(int fd, char *st_variable)
         read_size = read(fd, buff, BUFFER_SIZE);
     }
     free(buff);
-    if (read_size < 0)
+    if (read_size < 0) // read 가 실패하면 -1 반환
         return (0);
     return (st_variable);
 }
@@ -43,6 +73,8 @@ char    *read_file(int fd, char *st_variable)
 char	*get_next_line(int fd)
 {
     static char *st_variable
+    char        *line;
+    char        *tmp;
 
     if (fd < 0 || BUFFER_SIZE < 1)
         return (0);
@@ -50,6 +82,21 @@ char	*get_next_line(int fd)
     st_variable = read_file(fd, st_variable);
     if (!st_variable)
         return (0);
+    if (!*st_variable) {
+        free(st_variable);
+        return (0);
+    }
 
-    
+    line = get_line(st_variable); // st_variable를 가지고 있음
+    if (!line)
+    {
+        free(line);
+        return (0);
+    }
+    tmp = st_variable;
+    st_variable = ft_strdup(tmp + ft_strlen(line)); // st 받을 다음 주소로
+    free(tmp);
+    if (!st_variable)
+        return (0);
+    return (line);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,13 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 #include <stdlib.h>
 #include <unistd.h>
 
+char			*read_file(int fd, char *keep);
+char			*get_line(char *keep);
 char    *get_next_line(int fd);
 
+char			*ft_strdup(const char *s1);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlen(const char *s);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
